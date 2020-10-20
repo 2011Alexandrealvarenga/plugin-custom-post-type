e49cb38901ea64c99f890f24a2338d80bfedb495
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2020-10-20 18:56:38 -0200

    update

--- filmes-reviews.php
+++ filmes-reviews.php
@@ -9,34 +9,35 @@ Author URI:  http://boraviver.info
 Text Domain: filmes-reviews
 License: GPL2
 */
-class Filmes_reviews {
+
+class Filmes_reviews{
   private static $instance;
-
   public static function getInstance(){
-    if (self::$instance == NULL){
-        self::$instance = new self();
-      }
-      return self::$instance;
+    if(self::$instance == NULL){
+      self::$instance = new self();
     }
-    private function __construct(){
-      add_action('init', array($this,'register_post_type'));
-    }
-    function register_post_type(){
-      register_post_type('filmes_reviews',array(
-        'labels'      => array(
-          'name'          => 'Filmes Reviews',
-          'singular_name' => 'Filme Review',
-        ),
-
-        'description'     =>'Post para cadastro de reviews',
-        'supports'        => array(
+    return self::$instance;
+  }
+  private function __construct(){
+      add_action('init',array($this,'register_post_type'));
+  }
+  function register_post_type(){
+    register_post_type('filmes_reviews',array(
+      'labels'          => array(
+        'name'          => 'Filmes reviews',
+        'singular_name' => 'filmes review',        
+      ),
+        'description'   => 'post para cadastro de reviews',
+        'supports'      =>  array(
           'title','editor','excerpt','author','revisions','thumbnail','custom-fields',
+
         ),
-        'public'          =>  TRUE,
-        'menu_icon'       =>  'dashicons-format-video',
-        'menu_position'   =>  2,
-      ));
-    }
+        'public'        => TRUE,
+        'menu_position' =>  2,
+
+      
+    ));
+  }
 
 }
 Filmes_reviews::getInstance();
