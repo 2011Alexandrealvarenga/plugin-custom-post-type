60be32fd699bb7e0b03e4920178ee336c9672885
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2020-10-20 13:16:49 -0200

    add text type

---
+++ filmes-reviews.php
@@ -0,0 +1,10 @@
+/*
+Plugin Name: filmes review
+Plugin URI: http://exemplo.com
+Description: plugin para review filmes
+Version: 1.0
+Author: alexandre
+Author URI:  http://boraviver.info
+Text Domain: filmes-reviews
+License: GPL2
+*/
