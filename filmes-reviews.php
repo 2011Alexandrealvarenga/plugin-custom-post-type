8f584e8973857231036159906d7bed3b2b6369c2
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2020-10-20 13:20:38 -0200

    insert instance

--- filmes-reviews.php
+++ filmes-reviews.php
@@ -1,3 +1,4 @@
+<?php 
 /*
 Plugin Name: filmes review
 Plugin URI: http://exemplo.com
@@ -8,3 +9,18 @@ Author URI:  http://boraviver.info
 Text Domain: filmes-reviews
 License: GPL2
 */
+class Filmes_reviews {
+  private static $instance;
+
+  public static function getInstance(){
+    if (self::$instance == NULL){
+        self::$instance = new self();
+      }
+      return self::$instance;
+    }
+    private function __construct(){
+
+    }
+
+}
+Filmes_reviews::getInstance();
